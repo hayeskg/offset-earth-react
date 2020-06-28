e6bb031ad563cd35d3127bff6034a36a9d2ac12b
hayeskg <kristof.g.hayes@gmail.com>
2020-06-28 19:55:52 +0100

    Some directory clean up, getting data from API in the browser

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Offset Earth front-end challenge by K Hayes</h1>
-    </div>
-  );
+const axios = require('axios');
+
+
+
+class App extends React.Component {
+
+  state = {
+    treesData: []
+  }
+
+  getTreesData = () => {
+    axios.get('https://api.offset.earth/trees')
+      .then(({ data }) => {
+        this.setState({ treesData: [...data] })
+      })
+  }
+
+  componentDidMount() {
+    console.log('mounting...');
+    this.getTreesData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Offset Earth front-end challenge by K Hayes</h1>
+        {this.state.treesData.map((datapoint) => {
+          return (
+            <>
+              <p>Trees planted: {datapoint.value}</p>
+              <p>Date planted: {datapoint.createdAt}</p>
+            </>
+          )
+        })}
+      </div>
+    )
+  }
+
+
 }
+
 
 export default App;
