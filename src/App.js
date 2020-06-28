bf4708565db000f59dbf832cfadfc74f72888ffe
hayeskg <kristof.g.hayes@gmail.com>
2020-06-28 21:43:22 +0100

    date formatting and array sorting

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ class App extends React.Component {
     treesData: [],
     treesPerDay: [],
     dates: []
-
   }
 
   getTreesData = () => {
@@ -22,19 +21,42 @@ class App extends React.Component {
         this.setState({ treesData: [...data] })
       })
       .then(() => {
-        this.getXYArrays(this.state.treesData);
+        return this.handleDates(this.state.treesData)
       })
+      .then((arr) => {
+        console.dir(arr)
+        this.addTreesPerDay(arr);
+      })
+
   }
 
-  getXYArrays = (treesData) => {
-    let tempTreesPerDay = [];
-    let tempDates = []
+  handleDates = (treesData) => {
+    let formattedTreesData = [];
     treesData.map((dataObj) => {
-      tempTreesPerDay.push(dataObj.value);
-      tempDates.push(dataObj.createdAt);
-    })
-    console.log(tempTreesPerDay);
-    console.dir(tempDates);
+      let tempDate = new Date(dataObj.createdAt);
+      let tempYear = tempDate.getFullYear();
+      let tempMonth = tempDate.getMonth();
+      let tempDay = tempDate.getDate();
+      let myDate = tempYear + '-' + tempMonth + '-' + tempDay;
+      let outObj = { date: myDate, trees: dataObj.value };
+      formattedTreesData.push(outObj);
+    });
+    formattedTreesData.sort((a, b) => a.date - b.date);
+    return formattedTreesData;
+  }
+
+  addTreesPerDay = (formattedTreesData) => {
+    let finalTreesData = [];
+    let tempTreesSum = 0;
+    for (let i = 0; i < formattedTreesData.length - 1; i++) {
+      if (formattedTreesData[i].date === formattedTreesData[i + 1].date) {
+        tempTreesSum += formattedTreesData[i].trees;
+      } else {
+        finalTreesData.push({ date: formattedTreesData[i].date, trees: formattedTreesData[i].trees })
+      }
+      tempTreesSum = 0;
+    }
+    console.dir(finalTreesData)
   }
 
   componentDidMount() {
@@ -47,14 +69,14 @@ class App extends React.Component {
       <div className="App">
         <h1>Offset Earth front-end challenge by K Hayes</h1>
         <TreesChart />
-        {this.state.treesData.map((datapoint) => {
+        {/* {this.state.treesData.map((datapoint) => {
           return (
             <>
               <p>Trees planted: {datapoint.value}</p>
               <p>Date planted: {datapoint.createdAt}</p>
             </>
           )
-        })}
+        })} */}
       </div>
     )
   }
