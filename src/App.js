4a253c69f08490cdada08f45622ea40905ee5ee3
hayeskg <kristof.g.hayes@gmail.com>
2020-06-28 19:35:18 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Offset Earth front-end challenge by K Hayes</h1>
+    </div>
+  );
+}
+
+export default App;
